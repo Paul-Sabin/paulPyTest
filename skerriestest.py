21a16c943ad6cc01ac7f6f1cce5e2c0efd36ad43
Paul Sabin <psabin@gmail.com>
2023-08-15 12:21:53 +0200

    Changed number of steps

--- skerriestest.py
+++ skerriestest.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Create a range of numbers from 0 to 2 * Pi (100 steps)
-x = np.linspace(0, 2 * np.pi, 100)
+x = np.linspace(0, 2 * np.pi, 180)
 
 # Calculate the sine of these numbers
 y = np.sin(x)
